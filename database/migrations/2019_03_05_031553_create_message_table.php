5b0ad8cfafcccf19f878fa66956b7ec6e0595998
Cyrus <cyrus.anes95@gmail.com>
2019-03-05 13:26:56 +0800

    laravel first commit

---
+++ database/migrations/2019_03_05_031553_create_message_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->bigIncrements('message_id');
+            $table->string('message_name', 255);
+            $table->string('message_email', 255)->unique();
+            $table->string('message_title', 255);
+            $table->text('message_details');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
